100d2e176f572659f1772643714aac7aaad6748f
Muhammad Usman <179584824+usman-pirzada@users.noreply.github.com>
2025-11-05 08:54:00 +0500

    Add implementation of Binary Search Tree with search, insert, delete, and inorder display functions

---
+++ Trees/Binary Search Tree.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+using namespace std;
+
+class BST {
+private:
+    class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int data) : data(data), left(NULL), right(NULL) {}
+    };
+
+    Node* root;
+
+    static bool searchHelper(Node* root, int key) {
+        if(!root) {
+            return false;
+        }
+
+        if(key == root->data) {
+            return true;
+        } else if(key < root->data) {
+            return searchHelper(root->left, key);
+        } else {
+            return searchHelper(root->right, key);
+        }
+    }
+
+    static Node* insertHelper(Node* root, int data) {
+        if(!root) {
+            return new Node(data);
+        }
+
+        if(data < root->data) {
+            root->left = insertHelper(root->left, data);
+        } else {
+            root->right = insertHelper(root->right, data);
+        }
+
+        return root;
+    }
+
+    static Node* getInorderSuccessor(Node* root) {
+        while(!root && !root->left) {
+            root = root->left;
+        }
+
+        return root;
+    }
+
+    static Node* deleteNodeHelper(Node* root, int key) {
+        if(!root) {
+            return NULL;
+        }
+
+        if(key < root->data) {
+            root->left = deleteNodeHelper(root->left, key);
+        } else if(key > root->data) {
+            root->right = deleteNodeHelper(root->right, key);
+        } else {
+            if(root->left == NULL) {
+                Node* temp = root->right;
+                delete root;
+                return temp;
+            } else if(root->right == NULL) {
+                Node* temp = root->left;
+                delete root;
+                return temp;
+            } else {
+                Node* inorderSuccessor = getInorderSuccessor(root->right);
+                root->data = inorderSuccessor->data;
+                root->right = deleteNodeHelper(root->right, inorderSuccessor->data);    // delete the duplicate (as we have copied its data to root)
+            }
+        }
+    }
+
+    static void displayInorderHelper(Node* root) {
+        if(!root) {
+            return;
+        }
+
+        displayInorderHelper(root->left);
+        cout << root->data << " ";
+        displayInorderHelper(root->right);
+    }
+
+public:
+    BST() : root(NULL) {}
+
+    bool search(int key) {
+        return searchHelper(root, key);
+    }
+
+    void insert(int data) {
+        root = insertHelper(root, data);
+    }
+
+    void deleteNode(int key) {
+        root = deleteNodeHelper(root, key);
+    }
+
+    void displayInorder() {
+        displayInorderHelper(root);
+    }
+};
+
+int main() {
+    
+    BST myBST;
+    myBST.insert(54);
+    myBST.insert(76);
+    myBST.insert(29);
+    myBST.insert(66);
+    myBST.displayInorder();
+
+    cout << endl;
+    cout << myBST.search(76);
+    
+    return 0;
+}
